676cdd173dcc97a571aeb6abbaa695257430847e
prashant133 <dsrprasant@gmail.com>
2026-02-17 13:42:53 +0545

    array two sum quetsion and sum less tha target

---
+++ Array/moveZeros.js
@@ -0,0 +1,28 @@
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+
+
+nums = [0, 1, 0, 3, 12];
+
+const moveZeros = function(nums){
+    for(let i = 0 ; i < nums.length -1  ; i ++){
+      if(nums[i] === 0 ){
+        console.log(nums[i])
+      }
+    }
+
+}
+
+moveZeros(nums)
